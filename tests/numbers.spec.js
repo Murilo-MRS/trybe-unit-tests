6f7bfe870e920c02257957fe9a185f6669f17b75
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-19 16:59:25 -0300

    requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const numbers = require('../src/numbers');
+const numbers = require("../src/numbers");
 
 /*
   A função `numbers` recebe um array de tamanho variável e retorna `true` se todos os parâmetros forem do tipo 'number' e `false` caso contrário.
@@ -14,12 +14,16 @@ const numbers = require('../src/numbers');
 
 */
 
-describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+describe("2 - Implemente os casos de teste para a função `numbers`", () => {
+  it("Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário", () => {
+    // fail('Teste vazio!');
+    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true.
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([1, 'a', 3])).toBeFalsy();
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([' '])).toBeFalsy();
   });
 });
