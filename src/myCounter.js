05ab95941d8ec603c59b3985fc4b73974b4acb01
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-19 23:55:57 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let index2 = 2; index2 <= 3; index2 += 1) {
+      myArray.push(index2);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
